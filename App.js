c2defad6a225460bc2efe9c83c472ee4296a0d77
Saily Jadhav <mailstosaily@gmail.com>
2023-09-17 11:36:50 -0400

    React element and Functional component

--- App.js
+++ App.js
@@ -4,11 +4,29 @@ import ReactDOM from "react-dom/client";
 // React Element
 
 const heading1 = (
-  <h1 className="head1" tabIndex="4">
+  <h1 className="cid" tabIndex="4">
     This is heading 1 using React Element!
   </h1>
 );
 
+/** React Component: 2 ways
+ * Class based - old way
+ * Functional component - new way: Always name it with Capital letter
+ *
+ */
+
+// functional component
+const Heading2 = () => {
+  return <h2 id="tid">Heading 2 functional component!</h2>;
+};
+
+const container = (
+  <div id="container">
+    {heading1}
+    <Heading2 />
+  </div>
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading1);
+root.render(container);
