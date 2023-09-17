cd552cb3479413ec77a8679f1562ee82b76cc3c1
Saily Jadhav <mailstosaily@gmail.com>
2023-09-17 10:37:46 -0400

    Using React Element

--- App.js
+++ App.js
@@ -1,46 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "tid",
-    style: {
-      fontStyle: "italic",
-      fontFamily: "verdana",
-    },
-    key: "b",
-  },
-  "Hey Saily!"
+// React Element
+
+const heading1 = (
+  <h1 className="head1" tabIndex="4">
+    This is heading 1 using React Element!
+  </h1>
 );
-
-const heading2 = React.createElement(
-  "h1",
-  {
-    className: "cid",
-    style: {
-      fontStyle: "bold",
-    },
-    key: "a",
-    xyz: "ppp",
-  },
-  "How you doin!!"
-);
-
-// using traditional createElement method by React
-
-// const container = React.createElement("div", { id: "container" }, [
-//   heading1,
-//   heading2,
-// ]);
-
-// using JSX
-const container = <div id="container">{[heading1, heading2]}</div>;
-
-// For complex structure using JSX:
-
-console.log(heading2); /* its basically an object and not an HTML element */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(heading1);
