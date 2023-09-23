c3d0937a5846b014a8c62d8c31bf0b1dd5969758
Saily Jadhav <mailstosaily@gmail.com>
2023-09-23 16:50:36 -0400

    Displaying dynamic one card

--- App.js
+++ App.js
@@ -23,21 +23,101 @@ const Header = () => {
 };
 
 const styleCard = {
-  backgroundColor: "pink",
+  backgroundColor: "#80808038",
+  borderRadius: "16px",
+};
+
+const resObj = {
+  info: {
+    id: "78036",
+    name: "Burger King",
+    cloudinaryImageId: "e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    locality: "Central Plaza, Kalina",
+    areaName: "Santacruz East",
+    costForTwo: "₹350 for two",
+    cuisines: ["Burgers", "American"],
+    avgRating: 4.2,
+    feeDetails: {
+      restaurantId: "78036",
+      fees: [
+        {
+          name: "BASE_DISTANCE",
+          fee: 4100,
+        },
+        {
+          name: "BASE_TIME",
+        },
+        {
+          name: "ANCILLARY_SURGE_FEE",
+        },
+      ],
+      totalFee: 4100,
+    },
+    parentId: "166",
+    avgRatingString: "4.2",
+    totalRatingsString: "10K+",
+    sla: {
+      deliveryTime: 46,
+      lastMileTravel: 1.5,
+      serviceability: "SERVICEABLE",
+      slaString: "46 mins",
+      lastMileTravelString: "1.5 km",
+      iconType: "ICON_TYPE_EMPTY",
+    },
+    availability: {
+      nextCloseTime: "2023-09-24 03:45:00",
+      opened: true,
+    },
+    badges: {},
+    isOpen: true,
+    type: "F",
+    badgesV2: {
+      entityBadges: {
+        imageBased: {},
+        textBased: {},
+        textExtendedBadges: {},
+      },
+    },
+    aggregatedDiscountInfoV3: {
+      header: "EVERY ITEM",
+      subHeader: "@ ₹129",
+    },
+    differentiatedUi: {
+      displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+      differentiatedUiMediaDetails: {
+        mediaType: "ADS_MEDIA_ENUM_IMAGE",
+        lottie: {},
+        video: {},
+      },
+    },
+    reviewsSummary: {},
+    displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+    restaurantOfferPresentationInfo: {},
+  },
+  analytics: {},
+  cta: {
+    link: "https://www.swiggy.com/restaurants/burger-king-central-plaza-kalina-santacruz-east-mumbai-78036",
+    type: "WEBLINK",
+  },
 };
 
 const RestaurantCard = (props) => {
-  const { resname, cusines, rating } = props;
+  const { resData } = props;
+  console.log(resData.info);
   return (
     <div className="res-card" style={styleCard}>
       <img
         className="res-logo"
         alt="res-logo"
-        src="https://www.tastingtable.com/img/gallery/17-celebrity-chefs-and-their-favorite-fast-food-restaurants/l-intro-1674674335.jpg"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          resData.info.cloudinaryImageId
+        }
       />
-      <h3 style={{ color: "green" }}>{resname}</h3>
-      <h4>{cusines}</h4>
-      <h4>{rating} stars</h4>
+      <h3 style={{ color: "green" }}>{resData.info.name}</h3>
+      <h4>{resData.info.cuisines.join(", ")}</h4>
+      <h4>{resData.info.costForTwo}</h4>
+      <h4>{resData.info.avgRating} stars</h4>
       <h4>38 mins</h4>
     </div>
   );
@@ -47,15 +127,15 @@ const RestaurantCard = (props) => {
 const Body = () => {
   return (
     <div className="res-container">
-      <RestaurantCard
+      {/* <RestaurantCard
         resname="Meghana Food"
         cusines="Asian, North Indian"
         rating="4.5"
-      />
-      <RestaurantCard resname="KFC" cusines="Burger, Fast food" />
-      <RestaurantCard resname="McDonald" />
+      /> */}
+      <RestaurantCard resData={resObj} />
+      {/* <RestaurantCard resname="McDonald" />
       <RestaurantCard resname="Sai Biryani" />
-      <RestaurantCard resname="Chaitanya" />
+      <RestaurantCard resname="Chaitanya" /> */}
     </div>
   );
 };
