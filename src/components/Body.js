36b7036f442c49fee00e396ef7811d83c6819d08
Saily Jadhav <sailyjadhav1310@gmail.com>
2023-09-28 19:33:45 -0400

    useState hook and onClick method useage.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,39 @@
 import RestaurantCard from "./RestaurantCard";
 import myData from "../utils/mockData";
+import { useState } from "react";
 
 // Functional component
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurant] = useState(myData);
+
   return (
-    <div className="res-container">
-      {myData.map((restaurant) => (
-        <RestaurantCard resData={restaurant} key={restaurant.info.id} />
-      ))}
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.3
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+        <button
+          disabled={listOfRestaurants.length === myData.length}
+          onClick={() => {
+            setListOfRestaurant(myData);
+          }}
+        >
+          Remove the filter
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard resData={restaurant} key={restaurant.info.id} />
+        ))}
+      </div>
     </div>
   );
 };
